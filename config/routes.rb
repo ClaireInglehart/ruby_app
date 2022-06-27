bb3affe1ea4c6bbb9f8eac8b76ca2742937298de
Claire <claire.marie0425@gmail.com>
2022-06-27 10:49:38 -0700

    add pages, add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
-  get 'pages/about'
+  get 'history', to: 'pages#history'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'pages/history'
+  get 'account', to: 'pages#account'
 
   # Defines the root path route ("/")
   root "pages#home"
+
 end
